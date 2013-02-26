7538e7fb1b4bab55d3fae86c0950fb67a6f12897
Nancy Nguyen <nancy@nangoo.org>
2013-02-26 16:00:19 -0500

    Experimenting with BranchTexture (header)

--- GraphicPrototypes/src/BranchTexture.h
+++ GraphicPrototypes/src/BranchTexture.h
@@ -7,6 +7,10 @@ public:
 	void draw(ofPoint p1, ofPoint p2);
 	void draw(float x1, float y1, float x2, float y2);
 
+	int resolution;
+	float amplitude;
+	void setResolution(int resolution);
+
 	BranchTexture(string src);
 	~BranchTexture(void);
 };
