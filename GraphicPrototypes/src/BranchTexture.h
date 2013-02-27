63b9f100b6527ab7c2ab3f4a8f5809f266f46b97
Sunmock Yang <sunmock@live.ca>
2013-02-27 10:20:26 -0500

    GraphicsPrototypes - Updated with transform stuff

--- GraphicPrototypes/src/BranchTexture.h
+++ GraphicPrototypes/src/BranchTexture.h
@@ -7,11 +7,21 @@ public:
 	void draw(ofPoint p1, ofPoint p2);
 	void draw(float x1, float y1, float x2, float y2);
 
+<<<<<<< HEAD
 	int resolution;
+=======
+	float startWidth;
+	float endWidth;
+
+	int resolution;
+	int randomOffset;
+>>>>>>> Updated BranchTextures with transform stuff
 	float amplitude;
 	void setResolution(int resolution);
 
 	BranchTexture(string src);
 	~BranchTexture(void);
+private:
+	ofPath path;
 };
 
