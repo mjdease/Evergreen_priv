4fa6a2bfc7a730afb34d1ecfcbc5bfe840d55224
Matt Dease <mattdease+dev@gmail.com>
2013-04-18 14:55:23 -0400

    Added RGB LEDs to the ground arduino.

--- Evergreen_Arduino/arduino_ground/arduino_ground.ino
+++ Evergreen_Arduino/arduino_ground/arduino_ground.ino
@@ -1,5 +1,11 @@
 //RFID code is based off examples found at http://playground.arduino.cc/Learning/PRFID
 
+const int ShiftPWM_latchPin=10;
+const bool ShiftPWM_invertOutputs = true;
+const bool ShiftPWM_balanceLoad = false;
+
+#include <ShiftPWM.h> 
+
 #include <SoftwareSerial.h>
 
 #define PLANT "p:"
@@ -21,6 +27,16 @@
 #define ACC_PIN A0
 #define ACC_INTERVAL 300
 
+unsigned char maxBrightness = 255;
+unsigned char pwmFrequency = 75;
+int numRegisters = 2;
+
+//white, green, red, yellow
+//note anything less than fully on or off will interfere with the RFID reading
+// & the LEDs will flicker
+unsigned int r[4] = {255,   0,     255, 255};
+unsigned int g[4] = {255,   255,   0,   255};
+unsigned int b[4] = {255,   0,     0,   0};
 
 // RFID reader SOUT pin connected to Arduino pin8, nothing on pin 9.
 SoftwareSerial RFID = SoftwareSerial(8, 9); 
@@ -33,8 +49,11 @@ int emptyCount = 0;
 int pAcc;
 int accDiff = 0;
 
-void setup(){ 
+void setup(){  
   Serial.begin(115200);  // Hardware serial for Monitor 2400bps
+  
+  ShiftPWM.SetAmountOfRegisters(numRegisters);
+  ShiftPWM.Start(pwmFrequency,maxBrightness);
 
   pinMode(2,OUTPUT);       // Set digital pin 2 as OUTPUT to connect it to the RFID /ENABLE pin 
   digitalWrite(2, LOW);    // Activate the RFID reader 
@@ -48,6 +67,8 @@ void setup(){
 void loop(){ 
   unsigned long time = millis();
   
+  ShiftPWM.SetAllRGB(r[currentPlant], g[currentPlant], b[currentPlant]);
+  
   int accelerometer = analogRead(ACC_PIN);
   int diff = abs(accelerometer - pAcc);
   if(diff > 10){
@@ -56,7 +77,7 @@ void loop(){
   pAcc = accelerometer;
   
   if(time - pAccTime > ACC_INTERVAL){
-    if(accDiff > 35){
+    if(accDiff > 50){
       Serial.print(SHAKE);
       //value typically ranges from 0 - 150
       Serial.println(accDiff);
@@ -90,6 +111,7 @@ void loop(){
         if(bytesread == 10){
           code[bytesread] = '\0';
           emptyCount = 0;
+          //Serial.println(code);
           if(currentPlant != GRASS_VAL && strcmp(GRASS_ID, code) == 0){
             currentPlant = GRASS_VAL;
             Serial.print(PLANT);
@@ -131,3 +153,10 @@ void clearSerial(){
     RFID.read();
   }
 }
+
+//Wiring reference
+//accelerometer wires:
+//black gnd; z brown; y red; x orange; 3.3V yellow
+
+//rfid wires:
+//black gnd; white sout; grey enable; purple vcc0
